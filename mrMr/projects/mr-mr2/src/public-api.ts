bfb64616524dc088de2ee830d356a15f82ee1922
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-03-23 13:54:53 +0100

    Small changes to shared package.

--- mrMr/projects/mr-mr2/src/public-api.ts
+++ mrMr/projects/mr-mr2/src/public-api.ts
@@ -1,6 +1,20 @@
 /*
  * Public API Surface of mr-mr2
  */
+/* Module */
+export * from './lib/mr-mr2.module';
+
+/* Components  */
+export * from './lib/components/captain-canvas/captain-canvas.component';
+export * from './lib/components/rect-angular/rect-angular.component';
+export * from './lib/components/spinner/spinner.component';
+
+/* Pipes */
+export * from './lib/pipes/age.pipe';
+export * from './lib/pipes/bmi.pipe';
+export * from './lib/pipes/bmr.pipe';
+export * from './lib/pipes/filter.pipe';
+export * from './lib/pipes/sort.pipe';
 
 /* Services */
 export * from './lib/services/colour.service';
@@ -11,17 +25,3 @@ export * from './lib/services/http-interceptor.service';
 export * from './lib/services/image.service';
 export * from './lib/services/message.service';
 export * from './lib/services/parsing.service';
-/* Pipes */
-export * from './lib/pipes/age.pipe';
-export * from './lib/pipes/bmi.pipe';
-export * from './lib/pipes/bmr.pipe';
-export * from './lib/pipes/filter.pipe';
-export * from './lib/pipes/sort.pipe';
-
-/* Components  */
-export * from './lib/components/captain-canvas/captain-canvas.component';
-export * from './lib/components/rect-angular/rect-angular.component';
-export * from './lib/components/spinner/spinner.component';
-
-/* Module */
-export * from './lib/mr-mr2.module';
